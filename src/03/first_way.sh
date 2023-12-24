8e03c2dfd0fc0f103140d622f70df4171335bc4a
emmonbea <moskaleviluak@icloud.com>
2023-12-24 20:55:54 +0500

    feat(part_3): Реализовать очистку файловой системы по лог файлу

--- src/03/first_way.sh
+++ src/03/first_way.sh
@@ -2,5 +2,15 @@
 
 first_way () {
 
-echo "$1"
+local path=$1
+source ./validation.sh
+validation_first_way $path
+if [[ $? -eq 0 ]]; then
+    mapfile -t first_column < <(awk '{print $1}' "$path")
+    for value in "${first_column[@]}"; do
+        rm -rf $value
+    done
+else
+    echo "FAILURE"
+fi
 }
--- src/03/validation.sh
+++ src/03/validation.sh
@@ -3,14 +3,11 @@
 error_message () {
     echo "$1"
 }
-
-validation () {
-    error_code=0
-    if ! [[ $# -eq 1 ]]; then
-        error_message "Ошибка: Введите 1 параметр"
-        error_code=1
-    elif ! [[ $1 =~ ^[1-3]$ ]]; then
-        error_message "Ошибка: Выберите режим (1, 2 или 3)"
+validation_first_way () {
+    local error_code=0
+    local path=$1
+    if [[ ! -f $path ]]; then
+        error_message "Ошибка: Файл $path не найден"
         error_code=1
     fi
     return $error_code
