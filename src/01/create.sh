2928a93cc19139397eeed01579ea860efcb61fb8
emmonbea <moskaleviluak@icloud.com>
2023-12-27 18:14:22 +0500

    feat(Part_1): Добавить пользовательский интерфейс во всех частях скрипта

--- src/01/create.sh
+++ src/01/create.sh
@@ -14,35 +14,59 @@ create_folders_and_files () {
 
     local tmp_1=0
     local tmp_2=0
+    local start=1
+    local finish_1=$(( $count_folders * $count_files / 100 ))
+    local finish_2=$(( $count_folders * $count_files * 3 ))
     source ./generate_names.sh
-
+    draw_text "Формирование массивов уникальных имен:"
+    source ./status_bar.sh
+    process_status_bar $start $finish_1 &
+    local pid=$!
     if ! array_foldernames=($(generate_list_names $count_folders $list_folder_symbols $path)); then
-        echo "Ошибка: Невозможно создать $count_folders уникальных имен для каталогов"
+        error_message "Невозможно создать $count_folders уникальных имен для каталогов"
+        dialog_end
+        kill -SIGINT "$pid"
         exit 1
     fi
-
+    
     if ! array_filenames=($(generate_list_names $count_files $list_file_symbols $path)); then
-        echo "Ошибка: Невозможно создать $count_files уникальных имен для файлов"
+        error_message "Невозможно создать $count_files уникальных имен для файлов"
+        kill -SIGINT "$pid"
+        dialog_end
         exit 1
     fi
+    echo -e "\r${UP}${DELETE}\n${DELETE}"
+    echo -e "${UP}${UP}${UP}"
+    draw_text "Генерация файлов и каталогов:"
+    process_status_bar $start $finish_2 &
+    pid=$!
 
     for folder in "${array_foldernames[@]}"; do
         local dest_folder="$path${folder}_${date}"
-        mkdir "$dest_folder"
-        echo "По пути $path создан каталог ${folder}_${date} $(date +"%d.%m.%Y в %H:%M")" >> log.txt
-        ((tmp_1++))
-        for file in "${array_filenames[@]}"; do
-            local dest_file="${path}${folder}_${date}/${file}_${date}.${file_extension}"
-            fallocate -l $size "$dest_file"
-            echo -e "\tПо пути $dest_folder создан файл ${file}_${date}.${file_extension} размером ${size}b $(date +"%d.%m.%Y в %H:%M")" >> log.txt
-            ((tmp_2++))
-            if [[ $(available_memory) -le 1048576 ]]; then
-                echo "Ошибка: В системе остается менее 1 Гб свободного места. Создано $tmp_1 каталогов, $tmp_2 файлов"
-                exit 1
-            fi
-        done
+        if ! mkdir "$dest_folder" 2>/dev/null; then
+            error_message "Невозможно создать каталог по пути ${path}. Требуются root права"
+            kill -SIGINT "$pid"
+            dialog_end
+            exit 1
+        else
+            echo "${dest_folder} $(date +"%d.%m.%Y %H:%M")"  >> log.txt
+            ((tmp_1++))
+            for file in "${array_filenames[@]}"; do
+                local dest_file="${path}${folder}_${date}/${file}_${date}.${file_extension}"
+                fallocate -l $size "$dest_file"
+                echo  " ${dest_file} $(date +"%d.%m.%Y %H:%M") ${size}b" >> log.txt
+                ((tmp_2++))
+                if [[ $(available_memory) -le 1048576 ]]; then
+                    error_message "В системе остается менее 1 Гб свободного места. Создано $tmp_1 каталогов, $tmp_2 файлов"
+                    kill -SIGINT "$pid"
+                    dialog_end
+                    exit 1
+                fi
+            done
+        fi
     done
-    echo "Лог записан в файл log.txt" 
+    echo -e "\r${UP}${DELETE}\n${DELETE}"
+    echo -e "${UP}${UP}${UP}"
 }
 
 available_memory () {
--- src/01/main.sh
+++ src/01/main.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-source ./validation.sh
-start_time=$(date +%s)
+clear
 
+source ./validation.sh
+source ./dialog.sh
+
+dialog_hello
+start_time=$(date +%s)
 validation $@
 if [[ $? -eq 0 ]]; then
+    dialog_entered_parameters $@
     source ./create.sh
-    echo -e "\nВремя запуска скрипта: $(date +"%d.%m.%Y в %H:%M")" >> log.txt
+    draw_info "Время запуска скрипта: " "$(date -d "@$start_time" +"%d.%m.%Y %H:%M")\n"
     create_folders_and_files $@
+    end_time=$(date +%s)
+    execution_time=$((end_time - start_time))
+    echo -e -n "\r${DELETE}"
+    draw_info "Скрипт выполнился за " "$execution_time секунд\n"
+    draw_info "Лог записан в файл: " "log.txt"
+    dialog_end
 else
+    dialog_end
     exit 1
 fi
-
-end_time=$(date +%s)
-execution_time=$((end_time - start_time))
-echo "Скрипт выполнился за $execution_time секунд."
