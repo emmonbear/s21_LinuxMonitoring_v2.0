cb974552d9fa28e15486b02a70b0cce5a10a6064
emmonbea <moskaleviluak@icloud.com>
2023-12-29 17:06:41 +0500

    feat(Part_1): Записать аргументы строки в глобальные переменные

--- src/01/main.sh
+++ src/01/main.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-clear
+source ./dialog.sh
+dialog_hello
 
 source ./validation.sh
-source ./dialog.sh
-
-dialog_hello
-start_time=$(date +%s)
 validation $@
+
+readonly path=$1
+readonly count_folders=$2
+readonly list_folder_symbols=$3
+readonly count_files=$4
+readonly list_file_symbols=$(echo $5 | awk -F. '{print $1}')
+readonly file_extension=$(echo $5 | awk -F. '{print $2}')
+readonly size="${6%?}"
+
+readonly start_time=$(date +%s)
+
 if [[ $? -eq 0 ]]; then
     dialog_entered_parameters $@
-    source ./create.sh
     draw_info "Время запуска скрипта: " "$(date -d "@$start_time" +"%d.%m.%Y %H:%M")\n"
+
+    source ./create.sh
     create_folders_and_files $@
-    end_time=$(date +%s)
-    execution_time=$((end_time - start_time))
-    echo -e -n "\r${DELETE}"
+
+    readonly end_time=$(date +%s)
+    readonly execution_time=$((end_time - start_time))
+    
     draw_info "Скрипт выполнился за " "$execution_time секунд\n"
     draw_info "Лог записан в файл: " "log.txt"
     dialog_end
