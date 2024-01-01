53cadf4312b8535b5abf535a7a9f78763e6ab970
emmonbea <moskaleviluak@icloud.com>
2024-01-02 03:07:01 +0500

    fix(Part_2): Имена констант заглавными буквами

--- src/02/generate_names.sh
+++ src/02/generate_names.sh
@@ -7,18 +7,18 @@ generate_name () {
   local name=$1
   local name_length=0
 
-  local -r max_length=100
-  local -r min_length=5
+  local -r MAX_LENGTH=100
+  local -r MIN_LENGTH=5
   local -r cnt_symbols=${#name}
 
   local random_index
   local char
   local end_str
 
-  if ((${cnt_symbols} < ${min_length})); then
-    name_length=$(shuf -i ${min_length}-${max_length} -n 1)
+  if ((${cnt_symbols} < ${MIN_LENGTH})); then
+    name_length=$(shuf -i ${MIN_LENGTH}-${MAX_LENGTH} -n 1)
   else
-    name_length=$(shuf -i ${cnt_symbols}-${max_length} -n 1)
+    name_length=$(shuf -i ${cnt_symbols}-${MAX_LENGTH} -n 1)
   fi
 
   for ((i=0; i<${name_length} - ${cnt_symbols}; i++)); do
