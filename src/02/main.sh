994dfde756b152ad71a312f4054b9b101654ab4a
emmonbea <moskaleviluak@icloud.com>
2024-01-01 21:12:18 +0500

    fix(Part_2): Добавить переносы строк для читаемости

--- src/02/main.sh
+++ src/02/main.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-echo > log.txt
-source ./validation.sh
-source ./dialog.sh
-time_1=$(date +%s)
-dialog_hello
-validation $@
-if [[ $? -eq 0 ]]; then
-    dialog_entered_parameters $@
-    # source ./create.sh
-    # create $@
-    time_2=$(date +%s)
-    execution_time=$((time_2 - time_1))
+main () {
+  local -r time_1=$(date +%s)
+
+  source ./dialog.sh
+  dialog_hello
+
+  source ./validation.sh
+  validation "$@"
+
+  if [[ $? -eq 0 ]]; then
+    dialog_entered_parameters "$@"
+
+    source ./create.sh
+    create "$@"
+    
+    local -r time_2=$(date +%s)
+    local -r execution_time=$((time_2 - time_1))
+
     draw_info "Время запуска скрипта: " "$(date -d "@$time_1" +"%d.%m.%Y %H:%M")\n"
     draw_info "Время завершения скрипта: " "$(date -d "@$time_2" +"%d.%m.%Y %H:%M")\n"
     draw_info "Скрипт выполнился за " "$execution_time секунд\n"
+
     echo "Время запуска скрипта: " "$(date -d "@$time_1" +"%d.%m.%Y %H:%M")" >> log.txt
     echo "Время завершения скрипта: " "$(date -d "@$time_2" +"%d.%m.%Y %H:%M")" >> log.txt
+
     dialog_end
-else
+  else
     dialog_end
     exit 1
-fi
+  fi
+}
+
+main "$@"
