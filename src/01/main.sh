86b6349195e79d441656b41c67c2813cfd336190
emmonbea <moskaleviluak@icloud.com>
2024-01-01 16:00:50 +0500

    fix(Part_1):

--- src/01/main.sh
+++ src/01/main.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-source ./dialog.sh
-dialog_hello
+main () {
+  local start_time
+  readonly start_time=$(date +%s)
 
-source ./validation.sh
-validation $@
+  source ./dialog.sh
+  dialog_hello
 
-readonly path=$1
-readonly count_folders=$2
-readonly list_folder_symbols=$3
-readonly count_files=$4
-readonly list_file_symbols=$(echo $5 | awk -F. '{print $1}')
-readonly file_extension=$(echo $5 | awk -F. '{print $2}')
-readonly size="${6%?}"
+  source ./validation.sh
+  validation $@
 
-readonly start_time=$(date +%s)
-
-if [[ $? -eq 0 ]]; then
+  if [[ $? -eq 0 ]]; then
     dialog_entered_parameters $@
     draw_info "Время запуска скрипта: " "$(date -d "@$start_time" +"%d.%m.%Y %H:%M")\n"
 
-    source ./create.sh
-    create_folders_and_files $@
+    source ./create.sh 
+    create $@
 
-    readonly end_time=$(date +%s)
-    readonly execution_time=$((end_time - start_time))
+    local -r end_time=$(date +%s)
+    local -r execution_time=$((end_time - start_time))
     
     draw_info "Скрипт выполнился за " "$execution_time секунд\n"
     draw_info "Лог записан в файл: " "log.txt"
     dialog_end
-else
+  else
     dialog_end
     exit 1
-fi
+  fi
+}
+
+main "$@"
