8e4526b9e76bd9a1b3d22115c73e32ca68db89b9
emmonbea <moskaleviluak@icloud.com>
2024-01-01 16:13:08 +0500

    fix(Part_1): Глобальный фикс
    RANDOM поменялся на shuf. Эмпирически доказано, что RANDOM быстрее shuf, но менее элегантный
    
    Добавить описание функций

--- src/01/generate_names.sh
+++ src/01/generate_names.sh
@@ -1,57 +1,95 @@
 #!/bin/bash
 
+# Сформировать уникальное имя
+# Parameters:
+#   $1 - список букв, используемых в имени
 generate_name () {
-    local name=$1
-    local name_length=0
-    if ((${#name} < 4)); then
-        name_length=$((RANDOM % 35 + 4))
-    else
-        name_length=$((RANDOM % 35 + ${#name}))
-    fi
-    for ((j=0; j<$name_length - ${#name}; j++)); do
-        local random_index=$((RANDOM % ${#name}))
-        local char=${name:$random_index:1}
-        local end_str=${name:$random_index}
-        name=${name:0:$random_index}
-        name=${name}${char}${end_str}
-    done
-    echo $name
+  local name="$1"
+  local name_length=0
+  local -r cnt_symbols=${#name}
+
+  local -r max_length=100
+  local -r min_length=4
+
+  if ((${#name} < 4)); then
+    name_length=$(shuf -i $min_length-$max_length -n 1)
+  else
+    name_length=$(shuf -i ${#name}-50 -n 1)
+  fi
+  for ((j=0; j<${name_length} - ${cnt_symbols}; j++)); do
+    local random_index=$((RANDOM % ${#name}))
+    local char=${name:$random_index:1}
+    local end_str=${name:$random_index}
+    name=${name:0:$random_index}
+    name=${name}${char}${end_str}
+  done
+
+  echo $name
 }
 
+# Сканировать каталог по пути $1 и занести в массив все имена
+# Parameters:
+#   $1 - путь
 scan () {
-    local generated_names=()
-    for entry in "$1"/*; do
-        if [[ -e "$entry" ]]; then
-            name=$(basename "$entry")
-            name=${name%%_*}
-            generated_names+=(${name})
-        fi
-    done
-    echo ${generated_names[@]}
+  local generated_names=()
+  local name=""
+
+  for entry in "$1"/*; do
+    if [[ -e "$entry" ]]; then
+      name=$(basename "$entry")
+      name=${name%%_*}
+      generated_names+=(${name})
+    fi
+  done
+  
+  echo ${generated_names[@]}
 }
 
+# Генерация массива уникальных имен
+# Parameters:
+#   $1 - количество уникальных имен
+#   $2 - список букв, используемых в имени
+#   $3 - ссылка на массив, в который осуществляется запись
+# Returns:
+#   0 - успешное завершение программы
+#   1 - завершение программы при невозможности создать $1 уникальных имен
 generate_list_names () {
-    local count=$1
-    local list=$2
-    local path=$3
-    local max_retries=1000
-    local generated_array=()
-    local generated_names=$(scan $path)
-    for ((i=0; i<$count;i++)); do
-        local retries=0
-        while true; do
-            if [[ $retries -eq $max_retries ]]; then
-                return 1
-            else
-                local name=$(generate_name $list)
-                if [[ ! " ${generated_names[@]} " =~ " ${name} " ]]; then
-                    generated_names+=($name)
-                    generated_array+=($name)
-                    break
-                fi
-                ((retries++))
-            fi
-        done
+  local -r count="$1"
+  local -r list="$2"
+  local -n generated_array="$3"
+
+  local -r max_retries=1000
+
+  local generated_names=$(scan $path)
+  
+  for ((i=0; i<$count;i++)); do
+    local retries=0
+    while true; do
+      if [[ $retries -eq $max_retries ]]; then
+        echo ""
+        source ./validation.sh
+        delete_up
+        delete_up
+        error_message "Невозможно создать $count уникальных имен"
+        dialog_end
+        exit 1
+      else
+        local name=$(generate_name $list)
+        if [[ ! " ${generated_names[@]} " =~ " ${name} " ]]; then
+          generated_names+=($name)
+          generated_array+=($name)
+          break
+        fi
+        ((retries++))
+      fi
     done
-    echo "${generated_array[@]}"
+
+    source ./status_bar.sh
+    if [[ ${count} -eq 1 ]]; then
+      status_bar 1 1
+    else
+      status_bar ${i} $(( count - 1))
+    fi
+  done
+  echo ""
 }
