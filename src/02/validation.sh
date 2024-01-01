b0a069b6f01c177b26596a26e6fe56fd22a65e99
emmonbea <moskaleviluak@icloud.com>
2024-01-01 17:14:35 +0500

    feat(Part_2): Перенаправить ошибки в stderr, добавить описание функций

--- src/02/validation.sh
+++ src/02/validation.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
+# Вывод сообщения об ошибке и перенаправление его в stderr
+# Parameters:
+#   $1 - текст ошибке
 error_message () {
-    echo -e "${RED}Ошибка${RESET}: $1"
+  echo -e "${RED}Ошибка${RESET}: $1" >&2
 }
 
+# Валидация входных данных
+# Parameters:
+#   $@
+# Returns:
+#   0 - корректные аргументы, 1 - некорректные аргументы
 validation () {
-    local error_code=0
-    if [[ ! $# == 3 ]]; then
-        error_message "Введите 3 аргумента"
-        error_code=1
-    elif [[ ! $1 =~ ^([a-zA-Z]{2,7})$ ]]; then
-        error_message "Некорректный первый аргумент. Введите список букв латинского алфавита (от 2 до 7 букв)"
-        error_code=1
-    elif [[ ! $2 =~ ^([a-zA-Z]{2,7})\.([a-z]{1,3})$ ]]; then
-        error_message "Некорректный второй аргумент. Введите список букв латинского алфавита (от 2 до 7 букв для имени, от 1 до 3 для расширения)"
-        error_code=1
-    elif [[ ! $3 =~ ^([0-9]+Mb)$ ]]; then
-        error_message "Некорректный третий аргумент. Введите размер файла (в Мегабайтах, но не более 100)"
-        error_code=1
-    else
-        local param_3=$3
-        local size=${param_3%Mb}
-        if [[ $size -lt 1 || $size -gt 100 ]]; then
-            error_message "Некорректный третий аргумент. Введите размер файла (в Мегабайтах, но не более 100)"
-            error_code=1
-        fi
+  local error_code=0
+  if [[ ! $# == 3 ]]; then
+    error_message "Введите 3 аргумента"
+    error_code=1
+  elif [[ ! $1 =~ ^([a-zA-Z]{2,7})$ ]]; then
+    error_message "Некорректный первый аргумент. Введите список букв латинского алфавита (от 2 до 7 букв)"
+    error_code=1
+  elif [[ ! $2 =~ ^([a-zA-Z]{2,7})\.([a-z]{1,3})$ ]]; then
+    error_message "Некорректный второй аргумент. Введите список букв латинского алфавита (от 2 до 7 букв для имени, от 1 до 3 для расширения)"
+    error_code=1
+  elif [[ ! $3 =~ ^([0-9]+Mb)$ ]]; then
+    error_message "Некорректный третий аргумент. Введите размер файла (в Мегабайтах, но не более 100)"
+    error_code=1
+  else
+    local param_3=$3
+    local size=${param_3%Mb}
+    if [[ $size -lt 1 || $size -gt 100 ]]; then
+      error_message "Некорректный третий аргумент. Введите размер файла (в Мегабайтах, но не более 100)"
+      error_code=1
     fi
-    return $error_code
+  fi
+  return $error_code
 }
