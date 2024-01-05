46c9749276afba78a74445a506411dc50af4b805
emmonbea <moskaleviluak@icloud.com>
2024-01-05 13:10:06 +0500

    feat(Part_3): Добавлено много изменений
    Добавлено описание функций
    
    Переменные стали readonly
    
    Обновился статус-бар

--- src/03/first_way.sh
+++ src/03/first_way.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-first_way () {
-    source ./validation.sh
+# Первый способ очистки памяти
+# Parameters:
+#   $@ - Путь к лог файлу
+# Returns:
+#   0 - успешное завершение программы, 1 - неуспешное завершение (требуется root)
+first_way () {   
+  local path=$@
+  validation_first_way $path
+  if [[ $? -eq 0 ]]; then
+    source ./status_bar.sh
+
+    local cnt_status_bar=0
+    local -r finish=$(wc -l < "$path")
     
-    local path=$@
-
-    validation_first_way $path
-    if [[ $? -eq 0 ]]; then
-        source ./status_bar.sh
-
-        local start=1
-        local finish=$(wc -l < "$path")
-        
-        mapfile -t first_column < <(awk '{print $1}' "$path")
-        
-        process_status_bar $start $finish &
-        local pid=$!
-        for value in "${first_column[@]}"; do
-            if ! rm -rf $value 2>/dev/null && ! rmdir $value 2>/dev/null; then
-                echo -e "\n\n${RED}Ошибка${RESET}: Не удается очистить файловую систему"
-                echo "Возможно требуется запустить скрипт с root правами"
-                kill -SIGINT "$pid" ## попробовать без флага
-                return 1
-            fi
-        done
-        wait "$pid"
-    fi
+    mapfile -t first_column < <(awk '{print $1}' "$path")
+    
+    for value in "${first_column[@]}"; do
+      if ! rm -rf $value 2>/dev/null && ! rmdir $value 2>/dev/null; then
+        echo ""
+        error_message "Не удается очистить файловую систему"
+        echo "Возможно требуется запустить скрипт с root правами"
+        return 1
+      else
+        ((cnt_status_bar++))
+        status_bar $cnt_status_bar $finish 
+      fi
+    done
+    echo
+    delete_up
+    draw_text "Система была очищена"
+  fi
 }
-# qwer helps.txt 10Mb
-# qwer_271223
