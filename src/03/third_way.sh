9167111cb608ee8afc9fd1c5530d6b659cbfee25
emmonbea <moskaleviluak@icloud.com>
2024-01-05 13:14:09 +0500

    feat(Part_3): Добавлено много изменений
    Добавить описание функций
    
    Переменные readonly
    
    Обновить статус-бар

--- src/03/third_way.sh
+++ src/03/third_way.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
 
-third_way () {
-    local mask="$1"
-    IFS="_" read alphabet date <<< "$mask"
-    local regex=$(regular "$alphabet" "$date")
-    local result=($(find / 2>/dev/null | grep -E "$regex"))
-
-    local start=1
-    local finish=$(( ${#result[@]} ))
-    source ./status_bar.sh
-    process_status_bar $start $finish &
-    local pid=$!
-    for path in ${result[@]}; do
-        if ! rm -rf $path 2>/dev/null && ! rmdir $path 2>/dev/null; then
-            echo -e "\n\n${RED}Ошибка${RESET}: Не удается очистить файловую систему"
-            echo "Возможно требуется запустить скрипт с root правами"
-            kill -SIGINT "$pid" ## попробовать без флага
-            return 1
-        fi
-    done
-    wait "$pid"
-}
-
+# Формирование регуляроного выражения для grep
+# Parameters:
+#   $1 - Список букв латиницы
+#   $2 - Дата создания (ДДММГГ)
 regular () {
-    local alphabet="$1"
-    local date="$2"
-    local result=""
-    for ((i=0; i<${#alphabet}; i++)); do
-        result+=".*${alphabet:i:1}"
-    done
-    result+=".*_${date}.*"
-    echo "$result"
+  local alphabet="$1"
+  local date="$2"
+  local result=""
+  for ((i=0; i<${#alphabet}; i++)); do
+      result+=".*${alphabet:i:1}"
+  done
+  result+=".*_${date}.*"
+  echo "$result"
 }
-# asdf_271223
+
+# Третий способ очистки памяти
+# Parameters:
+#   $1 - Маска
+# Returns:
+#   0 - успешное завершение программы, 1 - неуспешное завершение (требуется root)
+third_way () {
+  local -r mask="$1"
+  IFS="_" read alphabet date <<< "$mask"
+  local -r regex=$(regular "$alphabet" "$date")
+  local -r result=($(find / 2>/dev/null | grep -E "$regex"))
+
+  source ./status_bar.sh
+  local -r finish=$(( ${#result[@]} ))
+  local cnt_status_bar=0
+  local pid=$!
+  for path in ${result[@]}; do
+    if ! rm -rf $path 2>/dev/null && ! rmdir $path 2>/dev/null; then
+      echo ""
+      error_message "Не удается очистить файловую систему"
+      echo "Возможно требуется запустить скрипт с root правами"
+      return 1
+    else
+      ((cnt_status_bar++))
+      status_bar $cnt_status_bar $finish
+    fi
+  done
+  echo
+  delete_up
+  draw_text "Система была очищена"
+}
+
