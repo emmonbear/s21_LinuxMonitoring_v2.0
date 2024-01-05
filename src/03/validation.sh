b04cc3d3ad99f527393cd34a1d4c67e3afd9b1dd
emmonbea <moskaleviluak@icloud.com>
2024-01-05 13:15:00 +0500

    feat(Part_3): Добавлено много изменений
    Сообщения об ошибках перенаправляются в stderr
    
    Добавлено описание функций
    
    Улучшена читаемость

--- src/03/validation.sh
+++ src/03/validation.sh
@@ -16,9 +16,11 @@ validation_first_way () {
   local error_code=0
   local path=$1
   if [[ ! $# -eq 1 ]]; then
+    echo ""
     error_message "Введите один аргумент (путь к лог файлу)"
     error_code=1
   elif [[ ! -f $path ]]; then
+    echo ""
     error_message "Файл $path не найден"
     error_code=1
   fi
@@ -29,7 +31,7 @@ validation_first_way () {
 # Parameters:
 #   $1 - дата
 # Returns:
-#   0 - корректные аргументы, 1 - некорректные аргументы
+#   0 - корректный аргумент, 1 - некорректный аргумент
 validation_date () {
   local error_code=0
 
@@ -37,7 +39,7 @@ validation_date () {
   local regex='^((0[1-9]|[1-2][0-9]|3[0-1])\.(0[1-9]|1[0-2])\.([0-9]{2}) ([0-1][0-9]|2[0-3])\:([0-5][0-9]))$'
 
   if [[ ! "$date" =~ $regex ]]; then
-    error_message "Введите дату в формате ДД.ММ.ГГ ЧЧ:ММ"
+    error_message "Введите дату в формате ДД.ММ.ГГ ЧЧ:ММ" #+
     error_code=1
   fi
 
@@ -56,7 +58,8 @@ validation_date_1 () {
   local -r date_2="$2"
 
   if [[ $(time_per_sec "$date_1") -gt $(time_per_sec "$date_2") ]]; then
-    error_message "Время начала генерации файлов не может быть больше времени окончания"
+    delete_up
+    error_message "Время начала генерации файлов не может быть больше времени окончания" #+
     error_code=1
   fi
 
@@ -74,10 +77,10 @@ validation_mask () {
   local -r mask="$1"
 
   if [[ ! $mask =~ ^([a-zA-Z]{2,7})\_((0[1-9]|[1-2][0-9]|3[0-1])(0[1-9]|1[0-2])([0-9]{2}))$ ]]; then
-    error_message "Введите аргумент в формате symbols_MMDDYY"
+    delete_up
+    error_message "Введите аргумент в формате symbols_MMDDYY" #+
     error_code=1
   fi
 
   return $error_code
 }
-
