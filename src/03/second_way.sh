18217336976f1ff862e80e58faf8c0e7472d9fa7
emmonbea <moskaleviluak@icloud.com>
2024-01-05 13:12:16 +0500

    feat(Part_3): Добавлено много изменений
    Добавлено описание функций
    
    Изменение статус-бара
    
    Переменные readonly

--- src/03/second_way.sh
+++ src/03/second_way.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-second_way () {
-    local start_time="$1"
-    local finish_time="$2"
-    local regex=$(regular "$start_time" "$finish_time")
-    local result=($(find / -newermt "@$(time_per_sec "$start_time")" ! -newermt "@$(time_per_sec "$finish_time")" 2>/dev/null | grep -E "$regex"))
-    local start=1
-    local finish=$(( ${#result[@]} ))
-    source ./status_bar.sh
-    process_status_bar $start $finish &
-    local pid=$!
-    for path in ${result[@]}; do
-        if ! rm -rf $path 2>/dev/null && ! rmdir $path 2>/dev/null; then
-            echo -e "\n\n${RED}Ошибка${RESET}: Не удается очистить файловую систему"
-            echo "Возможно требуется запустить скрипт с root правами"
-            kill -SIGINT "$pid" ## попробовать без флага
-            return 1
-        fi
-    done
-    wait "$pid"
-
-}
-
+# Время в секундах (с начала эпохи)
+# Parameters:
+#   $1 - Время в формате DD.MM.YY HH:MM
 time_per_sec () {
     local time="$1"
     IFS=". :" read -a time_array <<< "$time"
@@ -29,6 +10,11 @@ time_per_sec () {
     echo "$time_sec"
 }
 
+# Формирование регулярного выражения, для его последующего
+# использование функцией grep
+# Parameters:
+#   $1 - Время начала в секундах
+#   $2 - Время начала в секундах
 regular () {
     local start=$(time_per_sec "$1")
     local finish=$(time_per_sec "$2")
@@ -42,7 +28,36 @@ regular () {
     str="${str%?}"
     echo "$str"
 }
-# regular "27.12.23 01:32" "27.12.23 01:34"
-# 27.12.23 01:39
-# 27.12.23 01:46
 
+# Первый способ очистки памяти
+# Parameters:
+#   $1 - Время начала
+#   $2 - Время окончания
+# Returns:
+#   0 - успешное завершение программы, 1 - неуспешное завершение (требуется root)
+second_way () {
+  local -r start_time="$1"
+  local -r finish_time="$2"
+
+  local regex=$(regular "$start_time" "$finish_time")
+  local result=($(find / -newermt "@$(time_per_sec "$start_time")" ! -newermt "@$(time_per_sec "$finish_time")" 2>/dev/null | grep -E "$regex"))
+  
+  local cnt_status_bar=0
+  local -r finish=$(( ${#result[@]} ))
+
+  source ./status_bar.sh
+  for path in ${result[@]}; do
+    if ! rm -rf $path 2>/dev/null && ! rmdir $path 2>/dev/null; then
+      echo ""
+      error_message "Не удается очистить файловую систему"
+      echo "Возможно требуется запустить скрипт с root правами"
+      return 1
+    else
+      ((cnt_status_bar++))
+      status_bar $cnt_status_bar $finish 
+    fi
+  done
+  echo
+  delete_up
+  draw_text "Система была очищена"
+}
