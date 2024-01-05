1e0e35d0724f577e922d112667e408688505f0b5
emmonbea <moskaleviluak@icloud.com>
2024-01-05 14:49:22 +0500

    feat(Part_4): Реализовать функцию генерации случайного кода ответа

--- src/04/get_functions.sh
+++ src/04/get_functions.sh
@@ -7,10 +7,11 @@
 random () {
   local min=${1}
   local max=${2}
-  local number=0
+  local number
 
   number=$(shuf -i ${min}-${max} -n 1)
   readonly number
+
   echo "${number}"
 }
 
@@ -21,5 +22,18 @@ get_IP () {
     ip+="."$(random 0 255)
   done
   readonly ip
+
   echo "$ip"
 }
+
+# Сгенерировать случайный код ответа
+get_code () {
+  local -r codes=(200 201 400 401 403 404 500 501 502 503)
+
+  local index
+  index=$(random 0 9)
+  readonly index
+
+  echo ${codes[${index}]}
+}
+
