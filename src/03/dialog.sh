747740ae0b875db56105f626f4757bb7e7c0cd22
emmonbea <moskaleviluak@icloud.com>
2024-01-05 13:07:58 +0500

    feat(Part_3): Добавлено много изменений
    Переменные стали readonly
    
    Добавлено описание функций
    
    Добавлены функции по форматированию echo для читаемости
    
    Улучшена декомпозиция

--- src/03/dialog.sh
+++ src/03/dialog.sh
@@ -1,81 +1,129 @@
 #!/bin/bash
 
-YELLOW='\033[1;33m'
-GREEN='\033[1;36m'
-RESET='\033[0m'
-BOLD='\033[1m'
-RED='\033[1;31m'
-DELETE='\033[K'
-UP='\033[A'
+readonly YELLOW='\033[1;33m'
+readonly GREEN='\033[1;36m'
+readonly RESET='\033[0m'
+readonly BOLD='\033[1m'
+readonly RED='\033[1;31m'
+readonly DELETE='\033[K'
+readonly UP='\033[A'
 
+# Нарисовать рамку
+draw_frame () {
+  echo -e "${GREEN}${BOLD}${1}${RESET}"
+}
+
+# Выделить текст жирным
+draw_text () {
+  echo -e "${BOLD}${1}${RESET}"
+}
+
+# Выделить текст жирным и оставить курсор на этой строке
+draw_text_flag_n () {
+  echo -e -n "${BOLD}${1}${RESET}"
+}
+
+# 
 dialog_hello () {
-    clear
-
-    echo -e "${GREEN}${BOLD}====================================================================="
-    echo -e "${GREEN}${BOLD}                          Добро пожаловать!                          "
-    echo -e "${GREEN}${BOLD}=====================================================================${RESET}"
-    echo -e "${BOLD}Приветствую Вас в утилите по очистке файловой системы."
-    echo -e "Этот скрипт поможет вам избавиться от ненужных файлов и каталогов."
-    echo -e "\nВыберите один из трех способов очистки системы:${RESET}"
-    echo -e "1) По лог-файлу"
-    echo -e "2) По дате и времени создания"
-    echo -e "3) По маске имени (символы, нижнее подчёркивание и дата)"
+  clear
+  draw_frame "====================================================================="
+  draw_frame "                          Добро пожаловать!                          "
+  draw_frame "====================================================================="
+  draw_text "Приветствую Вас в утилите по очистке файловой системы."
+  draw_text "Этот скрипт поможет вам избавиться от ненужных файлов и каталогов."
+  echo ""
+  draw_text "Выберите один из трех способов очистки системы:"
+  echo " 1) По лог-файлу"
+  echo " 2) По дате и времени создания"
+  echo " 3) По маске имени (символы, нижнее подчёркивание и дата)"
+  echo ""
 }
 
+# Сместить курсор наверх и удалить строку
+delete_up () {
+  echo -e -n "\r${UP}${DELETE}"
+}
+
+# $1 жирный $2 желтый
+draw_info () {
+  echo -e "${BOLD}${1}${YELLOW}${2}${RESET}"
+}
+
+# Диалог: выбор способа очистки памяти
 dialog_choice_way () {
-    echo -e -n "\n${BOLD}Введите номер способа (1/2/3): "
-    read way
-    if [[ $way =~ ^[1-3]$ ]]; then
-        echo -e "${UP}${DELETE}Выбран способ: ${YELLOW}$way${RESET}\n"
-    else
-        echo -e "${UP}${DELETE}${UP}"
-    fi
+  draw_text "${BOLD}Введите номер способа (1/2/3): "
+  read way
+  if [[ $way =~ ^[1-3]$ ]]; then
+    delete_up
+    delete_up
+    draw_info "Выбран способ: " "$way"
+    echo ""
+  else
+    delete_up
+  fi
 }
 
+# Диалог: первый способ очистки памяти
+# Returns:
+#   0 - корректные аргументы, 1 - некорректные аргументы
 dialog_chosen_way_1 () {
-    echo -e -n "${BOLD}"
-    echo -e -n "Введите путь к лог файлу: "
-    read log_path
-    if [[ -z $log_path ]]; then
-        log_path="../02/log.txt"
-    fi
-    echo -e "${UP}${DELETE}${BOLD}Выбран файл: ${YELLOW}${log_path}${RESET}\n"
+  draw_text_flag_n "Введите путь к лог файлу: "
+  read log_path
+  if [[ -z $log_path ]]; then
+    log_path="../02/log.txt"
+  fi
+  delete_up
+  draw_info "Выбран файл: " "${log_path}"
+  echo ""
 }
 
+# Диалог: второй способ очистки памяти
+# Returns:
+#   0 - корректные аргументы, 1 - некорректные аргументы
 dialog_chosen_way_2 () {
-    local error_code=0
-    echo -e -n "${BOLD}Введите дату и время начала создания (ДД.ММ.ГГ ЧЧ:ММ): "
-    read start_time
-    validation_date "$start_time"
+  local error_code=0
+  draw_text_flag_n "Введите дату и время начала создания (ДД.ММ.ГГ ЧЧ:ММ): "
+  read start_time
+  validation_date "$start_time"
+  if [[ $? -eq 1 ]]; then
+    error_code=1
+  else
+    delete_up
+    draw_info "Начало создания файлов:    " "${start_time}"
+    draw_text_flag_n "Введите дату и время окончания создания (ДД.ММ.ГГ ЧЧ:ММ): "
+    read end_time
+    validation_date "$end_time"
     if [[ $? -eq 1 ]]; then
-        error_code=1
+      error_code=1
     else
-        echo -e "${UP}${DELETE}Начало создания файлов:    ${YELLOW}${start_time}${RESET}"
-        echo -e -n "${BOLD}Введите дату и время окончания создания (ДД.ММ.ГГ ЧЧ:ММ): "
-        read end_time
-        validation_date "$end_time"
-        if [[ $? -eq 1 ]]; then
-            error_code=1
-        else
-            echo -e "${UP}${DELETE}Окончание создания файлов: ${YELLOW}${end_time}${RESET}"
-            validation_date_1 "$start_time" "$end_time"
-            if [[ $? -eq 1 ]]; then
-                error_code=1
-            fi
-        fi
+      delete_up
+      draw_info "Окончание создания файлов: " "${end_time}"
+      validation_date_1 "$start_time" "$end_time"
+      if [[ $? -eq 1 ]]; then
+          error_code=1
+      fi
     fi
-    return $error_code
+  fi
+
+  return $error_code
 }
 
+# Диалог: третий способ очистки памяти
+# Returns:
+#   0 - корректные аргументы, 1 - некорректные аргументы
 dialog_chosen_way_3 () {
-    local error_code=0
-    echo -e -n "${BOLD}Введите маску имени в формате ${YELLOW}symbols_MMDDYY${RESET}: "
-    read name_mask
-    validation_mask "$name_mask"
-    if [[ $? -eq 1 ]]; then
-        error_code=1
-    else
-        echo -e "${UP}${DELETE}${BOLD}Выбрана маска: ${YELLOW}${name_mask}${RESET}\n"
-    fi
-    return $error_code
+  local error_code=0
+  draw_info "Введите маску имени в формате:" "symbols_MMDDYY"
+  read name_mask
+  validation_mask "$name_mask"
+  if [[ $? -eq 1 ]]; then
+    error_code=1
+  else
+    delete_up
+    delete_up
+    draw_info "Выбрана маска:" "${name_mask}"
+    echo ""
+  fi
+  return $error_code
 }
+
