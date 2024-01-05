8aa6b9a79c44b720a14eed98935dad4515f37b90
emmonbea <moskaleviluak@icloud.com>
2024-01-05 13:13:58 +0500

    feat(Part_3): Обновить статус-бар

--- src/03/status_bar.sh
+++ src/03/status_bar.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
 status_bar () {
-    local progress=$(((${1}*100/${2}*100)/100))
-    local finish=$(((${progress}*4)/10))
-    local unfinished=$((40-$finish))
+  local progress=$(((${1}*100/${2}*100)/100))
+  local finish=$(((${progress}*4)/10))
+  local unfinished=$((40-$finish))
 
-    local fill=$(printf "%${finish}s")
-    local empty=$(printf "%${unfinished}s")
+  local fill=$(printf "%${finish}s")
+  local empty=$(printf "%${unfinished}s")
 
-    printf "\r${BOLD}Прогресс${RESET}: [${fill// /#}${empty// /-}] ${progress}%%"
-}
-
-process_status_bar () {
-    local start=$1
-    local finish=$2
-
-    for number in $(seq ${start} ${finish}); do
-        status_bar $number $finish
-    done
-    sleep 0.1
-    echo -e "\r${DELETE}${BOLD}Система была очищена${RESET}"
+  printf "\r${BOLD}Прогресс${RESET}: [${fill// /#}${empty// /-}] ${progress}%%"
 }
