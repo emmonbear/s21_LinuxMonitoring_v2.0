def0c7a1d65873a32cd4b88101e6d0e9bcdc99ec
emmonbea <moskaleviluak@icloud.com>
2024-01-06 21:44:50 +0500

    feat(Part_6): Предусмотреть установку goaccess на различных дистрибутивах

---
+++ src/06/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+install () {
+  local distribution
+  distribution=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+  readonly distribution
+  
+  case ${distribution} in
+    *"Ubuntu"* | *"Debian")
+      sudo apt-get update
+      sudo apt-get install -y goaccess
+      ;;
+    *"Fedora"*)
+      yum install -y goaccess
+      ;;
+    *"Arch"*)
+      pacman -S goaccess
+      ;;
+    *"Gentoo"*)
+      emerge net-analyzer/goaccess
+      ;;
+    *)
+      error_message "Дистрибутив не поддерживается"
+      exit 1
+      ;;
+    esac
+}
